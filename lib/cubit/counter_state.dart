95867f698ae806bcc723b793bfea3ceb46709934
caothang1183 <caothang1183@gmail.com>
2021-06-10 12:04:42 +0700

    bloc test

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,8 +1,8 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
-  int counterValue;
-  bool wasIncremented;
+class CounterState extends Equatable {
+  final int counterValue;
+  final bool wasIncremented;
 
   CounterState({
     this.counterValue,
@@ -18,4 +18,7 @@ class CounterState {
       wasIncremented: wasIncremented ?? this.wasIncremented,
     );
   }
+
+  @override
+  List<Object> get props => [this.counterValue, this.wasIncremented];
 }
